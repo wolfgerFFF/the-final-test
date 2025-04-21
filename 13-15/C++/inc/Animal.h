1f40c9f3690ed4240c54708af68ca8f98eb3380c
wolfgerFFF <141361747+wolfgerFFF@users.noreply.github.com>
2025-04-21 19:41:56 +0300

    Update Animal.h

--- 13-15/C++/inc/Animal.h
+++ 13-15/C++/inc/Animal.h
@@ -1,1 +1,25 @@
 
+#ifndef ANIMAL_H
+#define ANIMAL_H
+
+#include <string>
+#include <vector>
+
+class Animal
+{
+protected:
+    std::string name;
+    std::string birthDate;
+    std::vector<std::string> commands;
+
+public:
+    Animal(const std::string &name, const std::string &birthDate);
+    virtual ~Animal() = default;
+
+    void addCommand(const std::string &command);
+    std::vector<std::string> getCommands() const;
+    std::string getName() const;
+    std::string getBirthDate() const;
+};
+
+#endif 
