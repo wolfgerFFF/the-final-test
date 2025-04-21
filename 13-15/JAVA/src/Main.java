00aa651c51fc992c9c00dc23e42fab6e6603be53
wolfgerFFF <141361747+wolfgerFFF@users.noreply.github.com>
2025-04-21 20:43:31 +0300

    Create Main.java

---
+++ 13-15/JAVA/src/Main.java
@@ -0,0 +1,8 @@
+import registry.AnimalRegistry;
+
+public class Main {
+    public static void main(String[] args) {
+        AnimalRegistry registry = new AnimalRegistry();
+        registry.showMenu();
+    }
+}
