3f2e873a6f5a6074428a44b1e72643eeaca20f6b
wolfgerFFF <141361747+wolfgerFFF@users.noreply.github.com>
2025-04-21 21:15:36 +0300

    Update DomesticAnimal.java

--- 13-15/JAVA/src/animals/domestic/DomesticAnimal.java
+++ 13-15/JAVA/src/animals/domestic/DomesticAnimal.java
@@ -1,37 +1,9 @@
-// DomesticAnimal.java
 package animals.domestic;
 
 import animals.Animal;
 
 public class DomesticAnimal extends Animal {
     public DomesticAnimal(String name, String birthDate) {
-        super(name, birthDate);
-    }
-}
-
-// Dog.java
-package animals.domestic;
-
-public class Dog extends DomesticAnimal {
-    public Dog(String name, String birthDate) {
-        super(name, birthDate);
-    }
-}
-
-// Cat.java
-package animals.domestic;
-
-public class Cat extends DomesticAnimal {
-    public Cat(String name, String birthDate) {
-        super(name, birthDate);
-    }
-}
-
-// Hamster.java
-package animals.domestic;
-
-public class Hamster extends DomesticAnimal {
-    public Hamster(String name, String birthDate) {
         super(name, birthDate);
     }
 }
